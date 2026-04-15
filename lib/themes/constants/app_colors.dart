297e884c675019fc8b799473e304c52eea936b05
Jasmine Gargoles Magadan <jgmagadan@up.edu.ph>
2026-04-15 21:57:40 +0800

    meta: added color constants for pink theme

--- lib/themes/constants/app_colors.dart
+++ lib/themes/constants/app_colors.dart
@@ -36,7 +36,7 @@ class _BlueColorCollection{
   final bubble = const Color (0xFFC3D4F0);
   final activeElement = const Color (0xFF1E2A3A);
   final themeItemIcon = const Color(0xFF8694AD);
-  final dateContainer = const Color(0xFF)
+  final dateContainer = const Color(0xFF);
 
   // text colors
   final pageTitleText = const Color(0xFF1C2340);
@@ -48,24 +48,25 @@ class _PinkColorCollection{
   const _PinkColorCollection();
 
   // background colors for page, card, and field
-  final pageBackground = const Color(0xFFDDE4EE);
-  final cardBackground = const Color(0xFFBFC8D6);
+  final pageBackground = const Color(0xFFF7E7FF);
+  final cardBackground = const Color(0xFFDDBCCD);
   final fieldBackground =  const Color(0xFFFFFFFF); // white
 
   // widget colors
-  final lightDivider = const Color(0xFFD0D7E2);
-  final darkDivider = const Color(0xFF6C80A4);
-  final icon =  const Color(0xFF8694AD);
+  final lightDivider = const Color(0xFFF9D5EB);
+  final darkDivider = const Color(0xFFC282A3);
+  final icon =  const Color(0xFF3A1E2E);
   final container = const Color(0xFFC7D7F0); //container for profile and settings page
   final bubble = const Color (0xFFF6BCDF);
-  final confirmButton = const Color (0xFF1E2A3A);
-  final cancelButton = const Color(0xFFFFFFFF);
+  final activeElement = const Color (0xFF3A1E2E);
   final themeItemIcon = const Color(0xFFF0C3DE);
+  final dateContainer = const Color(0xFFF0C3DE);
+
 
   // text colors
-  final pageTitleText = const Color(0xFF1C2340);
-  final bodyText = const Color(0xFF2E3A59);
-  final hintText = const Color(0xFF8A9BB5);
+  final pageTitleText = const Color(0xFF3A1E2E);
+  final bodyText = const Color(0xFF84355E);
+  final hintText = const Color(0xFFC282A3);
 }
 
 class _GreenColorCollection{
