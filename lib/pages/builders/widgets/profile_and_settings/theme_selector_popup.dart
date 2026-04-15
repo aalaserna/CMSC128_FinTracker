555cba5f828ca31ef98edecf51829df36787e336
Jasmine Gargoles Magadan <jgmagadan@up.edu.ph>
2026-04-15 21:46:53 +0800

    feat: fully functional theme selector

--- lib/pages/builders/widgets/profile_and_settings/theme_selector_popup.dart
+++ lib/pages/builders/widgets/profile_and_settings/theme_selector_popup.dart
@@ -1,37 +1,64 @@
-import 'package:fins/utils/app_colors.dart';
+import 'package:fins/themes/constants/app_colors.dart';
+import 'package:fins/themes/logic/app_themes.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
-class ThemeSelectorPopup extends StatelessWidget{
-  final dynamic theme;
+class ThemeSelectorPopup extends StatefulWidget{
+  final AppThemeType initialTheme;
+  final Function(AppThemeType) onConfirm; // save chosen theme
+
 
   const ThemeSelectorPopup({
     super.key,
-    required this.theme
+    required this.initialTheme,
+    required this.onConfirm,
   });
 
   @override
+  State<ThemeSelectorPopup> createState() => _ThemeSelectorPopupState();
+}
+
+class _ThemeSelectorPopupState extends State<ThemeSelectorPopup> {
+
+  late AppThemeType _tempSelectedTheme;
+
+  @override
+  void initState() {
+    super.initState();
+    _tempSelectedTheme = widget.initialTheme;
+  }
+
+  void _handleThemeSelection(AppThemeType choice) {
+    setState(() {
+      _tempSelectedTheme = choice;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final hasThemeChoiceChanged = _tempSelectedTheme != widget.initialTheme;
+
     return SimpleDialog(
-      backgroundColor: theme.pageBackground,
+      backgroundColor: theme.scaffoldBackgroundColor,
       children: [
         Padding(
           padding: const EdgeInsets.all(15),
           child: SizedBox(
-            width: double.maxFinite, // Ensure it takes the dialog width
+            width: double.maxFinite,
             child: GridView.count(
               shrinkWrap: true,
-              crossAxisCount: 3, // Forces exactly 3 per row
+              crossAxisCount: 3,
               mainAxisSpacing: 20,
               crossAxisSpacing: 20,
               physics: const NeverScrollableScrollPhysics(),
-              children: [
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-                _buildThemeChoice(themeChoice: AppColors.blue, currentTheme: theme),
-              ],
+              children: AppThemeType.values.map((type) {
+                return _buildThemeChoice(
+                  type: type,
+                  isSelected: _tempSelectedTheme == type,
+                  onTap: () => setState(() => _tempSelectedTheme = type),
+                );
+              }).toList(),
             ),
           )
         ),
@@ -45,16 +72,16 @@ class ThemeSelectorPopup extends StatelessWidget{
                 SizedBox(
                   height: 52,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () => Navigator.pop(context),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: theme.fieldBackground,
-                      foregroundColor: theme.pageTitleText,
+                      backgroundColor: context.surface,
+                      foregroundColor: context.onPrimary,
                       elevation: 0,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    child: const Text(
+                    child: Text(
                       'Cancel',
                       style: TextStyle(
                         fontSize: 16,
@@ -71,16 +98,21 @@ class ThemeSelectorPopup extends StatelessWidget{
                 SizedBox(
                   height: 52,
                   child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: theme.pageTitleText,
-                      foregroundColor: theme.fieldBackground,
+                    onPressed: hasThemeChoiceChanged
+                      ? () {
+                      widget.onConfirm(_tempSelectedTheme);
+                      Navigator.pop(context, _tempSelectedTheme);
+                      }
+                      : null,
+                    style: ElevatedButton.styleFrom( // confirm button is disabled unless selected theme is not initial theme
+                      backgroundColor: hasThemeChoiceChanged ? context.primary :  context.primary.withOpacity(0.8),
+                      foregroundColor: hasThemeChoiceChanged ? context.hintText :  context.hintText.withOpacity(0.8),
                       elevation: 0,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    child: const Text(
+                    child: Text(
                       'Confirm',
                       style: TextStyle(
                         fontSize: 16,
@@ -101,23 +133,27 @@ class ThemeSelectorPopup extends StatelessWidget{
 }
 
 Widget _buildThemeChoice({
-  required dynamic themeChoice,
-  required dynamic currentTheme,
-  bool isSelected = false,
+  required AppThemeType type,
+  required bool isSelected,
+  required VoidCallback onTap,
 }){
+  final previewTheme = getTheme(type);
 
-  return Container(
-    padding: const EdgeInsets.all(2),
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      border: Border.all(
-        color: isSelected ? currentTheme.pageBackground : Colors.black,
-        width: 2,
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? previewTheme.colorScheme.onPrimary : previewTheme.colorScheme.onPrimary.withOpacity(0.3),
+          width: 2,
+        ),
       ),
-    ),
-    child: CircleAvatar(
-      radius: 20,
-      backgroundColor: currentTheme.pageBackground,
+      child: CircleAvatar(
+        radius: 20,
+        backgroundColor: previewTheme.extension<ExtraColors>()!.themeItemIcon,
+      ),
     ),
   );
 }
---
+++ lib/themes/constants/app_colors.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+/*
+==================================================================================
+   This is a dumb file containing the app colors for all 6 selected themes.
+   It will be used by the app themes, which will be the one containing the logic.
+   If you ever need to change a color or whatever, go to this file.
+==================================================================================
+*/
+
+class AppColors {
+
+  AppColors._();
+
+  static const blue = _BlueColorCollection(); // DEFAULT BLUE THEME
+  static const pink = _PinkColorCollection();
+  static const green = _GreenColorCollection();
+  static const orange = _OrangeColorCollection();
+  static const cyan = _CyanColorCollection();
+  static const purple = _PurpleColorCollection();
+}
+
+class _BlueColorCollection{
+  const _BlueColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFC3D4F0);
+  final activeElement = const Color (0xFF1E2A3A);
+  final themeItemIcon = const Color(0xFF8694AD);
+  final dateContainer = const Color(0xFF)
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
+class _PinkColorCollection{
+  const _PinkColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFF6BCDF);
+  final confirmButton = const Color (0xFF1E2A3A);
+  final cancelButton = const Color(0xFFFFFFFF);
+  final themeItemIcon = const Color(0xFFF0C3DE);
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
+class _GreenColorCollection{
+  const _GreenColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFC3D4F0);
+  final confirmButton = const Color (0xFF1E2A3A);
+  final cancelButton = const Color(0xFFFFFFFF);
+  final themeItemIcon = const Color(0xFF5CBB5C);
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
+class _OrangeColorCollection{
+  const _OrangeColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFC3D4F0);
+  final confirmButton = const Color (0xFF1E2A3A);
+  final cancelButton = const Color(0xFFFFFFFF);
+  final themeItemIcon = const Color(0xFFE5965D);
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
+class _CyanColorCollection{
+  const _CyanColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFC3D4F0);
+  final confirmButton = const Color (0xFF1E2A3A);
+  final cancelButton = const Color(0xFFFFFFFF);
+  final themeItemIcon = const Color(0xFF5CBBB3);
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
+class _PurpleColorCollection{
+  const _PurpleColorCollection();
+
+  // background colors for page, card, and field
+  final pageBackground = const Color(0xFFDDE4EE);
+  final cardBackground = const Color(0xFFBFC8D6);
+  final fieldBackground =  const Color(0xFFFFFFFF); // white
+
+  // widget colors
+  final lightDivider = const Color(0xFFD0D7E2);
+  final darkDivider = const Color(0xFF6C80A4);
+  final icon =  const Color(0xFF8694AD);
+  final container = const Color(0xFFC7D7F0); //container for profile and settings page
+  final bubble = const Color (0xFFC3D4F0);
+  final confirmButton = const Color (0xFF1E2A3A);
+  final cancelButton = const Color(0xFFFFFFFF);
+  final themeItemIcon = const Color(0xFF985CBB);
+
+  // text colors
+  final pageTitleText = const Color(0xFF1C2340);
+  final bodyText = const Color(0xFF2E3A59);
+  final hintText = const Color(0xFF8A9BB5);
+}
+
